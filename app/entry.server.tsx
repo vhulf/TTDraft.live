d1042cc227ef731077366178eecff9dc6561bae5
vhulf <emma.lee.latuszek@gmail.com>
2025-05-27 11:33:26 -0600

    fixed renderToReadableStream error

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,7 +1,7 @@
 import type { AppLoadContext, EntryContext } from "react-router";
 import { ServerRouter } from "react-router";
 import { isbot } from "isbot";
-import { renderToReadableStream } from "react-dom/server";
+import { renderToReadableStream } from "react-dom/server.browser";
 
 export default async function handleRequest(
   request: Request,
